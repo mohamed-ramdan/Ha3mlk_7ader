6e9455981d8ae20d187ac19aef73d8df66531163
Heshamwhite <heshamwhite@gmail.com>
2015-02-27 15:30:37 +0200

    The PHP Project

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<!--
+Ha3melk 7ader is cafe management web application.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            // just for testing
+            echo 'Ha3mlek 7ader launching';
+        ?>
+    </body>
+</html>
