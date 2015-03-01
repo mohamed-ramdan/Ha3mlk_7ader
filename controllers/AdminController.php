598d12f679ed1a7f641de0766da05a503a58c5fb
mohamedramadan <mohamedramadan@dell>
2015-03-01 23:03:07 +0200

    function getUnfinishedOrders , AdminController

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -1,8 +1,56 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/*
+      ***---((---((  __PLEASE READ__  ))---))---***
+ * ====================================================
+ * In views unfinishedorders.php file needs to be start from
+ * by this link    http://localhost/Ha3mlk_7ader/views/unfinishedorders.php
+ * then submit to redirect to adminController where the constrcutor calls unFinishedOrders function
+ * that call ORM select function where order status = undone then var_dumb the result considering creating
+ * an object from the class directly under the constructor 
+ * 
+ * Note: database tables has a valid data inserted for test
+ * Note: mysql connection parameters had been set ('localhost','root','admin','ha3mlk7aderdb'); in ORM
+ * 
+ * The Expected result: on press submit key go to admin controller and see the array printed or even NULL value
+ * The Real result: Blank page wit NO error defined
+ *  */
+
+
+// Include Needed Models 
+include_once '../models/ORM.php'.'../models/Order.php'.'../models/User.php'.'../models/Category.php'.'../models/Product.php';
+
+
+class AdminController
+{
+ 
+    /**
+     * getUnFinishedOrders is a function that get all unfinished orders
+     * Using ORM select method
+     * @param void 
+     * @return array Include unfinished orders
+     */
+    function getUnFinishedOrders()
+    {
+        echo 'hi there, inside my function';
+        $orm = new ORM();
+        $orm->setTable('h3mlk7aderdb.cafeOrder');
+        $unFinishedOrders=array();
+        $unFinishedOrders = $orm->select($data=array("status"=>"undone"));
+        var_dump($unFinishedOrders);
+        header("Location:unfinishedorders.php?orders=$unFinishedOrders");
+        
+    }
+    
+    protected function __construct() {
+        
+        echo 'inside constructor';
+        $this->getUnFinishedOrders();
+        
+    }
+    
+}
+$varAdmin = new AdminController();
+
+?>
 
